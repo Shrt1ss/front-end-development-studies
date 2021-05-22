9be1a9ae022da1fadacd55ae5bb86873b537848b
Shrt1ss <alphastra1k25@gmail.com>
2021-05-22 16:21:39 +0300

    Watching is finished. Now I start my homework

--- JavaScript/course/mapAndFilter/mapAndFilter.js
+++ JavaScript/course/mapAndFilter/mapAndFilter.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    /* let testArr = [1337, 228, 1441, 1990, -3, -47, 21, 22, 35, 48];
+    let mapVar = testArr.map(function (item, index, array) {
+        console.log(item);
+        let multiply = item * 5;
+        return multiply;
+    });
+    console.log(mapVar);
+    let filterVar = testArr.filter(function (item, index, array) {
+        if (item % 2 == 0) {
+            return true;
+        };
+    });
+
+    console.log(filterVar); */
+
+
 });
