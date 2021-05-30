76bdab78e9fb7fdd75defe1bf721a3de100629aa
Shrt1ss <alphastra1k25@gmail.com>
2021-05-30 11:38:10 +0300

    Lesson about local storage is finished. Homework is completed.

--- JavaScript/course/localStorage/changingLS.js
+++ JavaScript/course/localStorage/changingLS.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let count = 0;
+    /* let count = 0;
 
     document.querySelector('.local-storage-changing').onclick = () => {
         count++;
         localStorage.setItem('test', count);
     };
 
+    document.querySelector('.local-storage-clear').onclick = () => {
+        count = 0;
+        localStorage.clear();
+    }
+ */
 });
--- JavaScript/course/localStorage/localStorage.js
+++ JavaScript/course/localStorage/localStorage.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    window.addEventListener('storage', function (e) {
+    /* window.addEventListener('storage', function (e) {
+        let count = localStorage.getItem('test');
         console.log('change');
-    });
+        document.querySelector('.out').textContent = count;
+    }); */
     /* localStorage.setItem('data', 'USER NUMBER 1');
     console.log(localStorage.getItem('data')); */
 
-    const testData = [3, 4, 5];
+    /* const testData = [3, 4, 5];
 
     localStorage.setItem('test', JSON.stringify(testData));
 
@@ -33,5 +35,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log(d);
     console.log(d[0]);
-    console.log(typeof d);
+    console.log(typeof d); */
+
+    document.querySelectorAll('.local-storage-changing').forEach((elem, index) => {
+        elem.addEventListener('click', (event) => {
+
+            if (index === 0) {
+
+                localStorage.clear();
+                localStorage.setItem('5', 11);
+
+                let check = JSON.parse(localStorage.getItem('5'));
+                console.log(typeof check);
+            }
+            else if (index === 1) {
+                localStorage.clear();
+                localStorage.setItem('array', JSON.stringify([7, 6, 9, 8]));
+
+                var parse = localStorage.getItem('array');
+                parse = JSON.parse(parse);
+
+                console.log(parse);
+                console.log(typeof parse);
+            }
+            else if (index === 2) {
+
+                let str = '';
+                let helpArr = [];
+                console.log(typeof localStorage.getItem('array'));
+
+                helpArr = localStorage.getItem('array').split('');
+                helpArr.shift();
+                helpArr.pop();
+
+                let join = helpArr.join('');
+                join = join.split(',');
+
+                document.querySelector('.description').innerHTML = `${str}`;
+
+                for (let item of join) {
+                    str += `array: ${item}<br>`;
+                    console.log('str: ', str);
+                };
+
+                document.querySelector('.out').innerHTML = str;
+
+            };
+        });
+    });
 });
